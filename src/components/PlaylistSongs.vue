cccefb7b6d5c391b40777bff31f1d02eb2aed6c6
marrevn <sveta.i.volhina@gmail.com>
2025-11-17 13:56:23 +0500

    song component

--- src/components/PlaylistSongs.vue
+++ src/components/PlaylistSongs.vue
@@ -5,16 +5,17 @@
 <template>
     <div class="playlist-songs">
         <Song title="Трек №1" :artists="['Исполнитель №1', 'Исполнитель №2']"/>
-        <Song title="Трек №2"/>
-        <Song title="Трек №3"/>
-        <Song title="Трек №4"/>
-        <Song title="Трек №5"/>
-        <Song title="Трек №6"/>
+        <Song title="Трек №1" :artists="['Исполнитель №1', 'Исполнитель №2']"/>
+        <Song title="Трек №1" :artists="['Исполнитель №1', 'Исполнитель №2']"/>
+        <Song title="Трек №1" :artists="['Исполнитель №1', 'Исполнитель №2']"/>
     </div>
 </template>
 
 <style scoped>
     .playlist-songs{
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
         background-color: rgb(55, 55, 55);
         flex-grow: 1;
     }
--- src/components/Song.vue
+++ src/components/Song.vue
@@ -1,24 +1,69 @@
 <script setup>
     defineProps({
         title: String,
-        artists: Array
+        artists: Array,
+        // length: String
     })
 </script>
 
 <template>
     <div class="song">
         <div class="image">
-            <img src="" alt="Изображение плейлиста">
+            <!-- <img src="" alt="Изображение плейлиста"> -->
         </div>
-        <div>
+        <div class="song-info">
             <span>{{title}}</span>
-            <span class="additional-info" v-for="artist in artists">{{ artist }}</span>
+            <span class="additional-info">{{ artists.join(', ') }}</span>
+        </div>
+        <div class="song-actions">
+            <span>3:40</span>
+            <div class="play-button">
+                
+            </div>
+            <div class="misc-actions">
+
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
     .song{
-        
+        display: flex;
+        gap: 10px;
+        /* width: 100%; */
+        padding: 5px 10px;
+
+        .image{
+            background-color: lightgray;
+            width: 50px;
+            height: 50px;
+            border-radius: 5px;
+        }
+
+        .song-info{
+            display: flex;
+            flex-direction: column;
+            align-self: center;
+            gap: 3px;
+            flex-grow: 1;
+        }
+
+        .song-actions{
+            display: flex;
+            align-self: center;
+
+            span{
+                font-size: 12px;
+            }
+
+            .play-button{
+                
+            }
+
+            .misc-buttons{
+
+            }
+        }
     }
 </style>
