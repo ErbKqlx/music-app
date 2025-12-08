47d2d2d1338cb1540d693b9952d4519968c83e24
Erbeen <Error5455@yandex.ru>
2025-12-08 10:45:38 +0500

    remake playlist page

--- src/components/SongsList.vue
+++ src/components/SongsList.vue
@@ -4,13 +4,14 @@
 
 <template>
     <div class="songs-list">
-        <Song 
+        <slot></slot>
+        <!-- <Song 
             title="Трек №1"
             :artists="['Исполнитель №1', 'Исполнитель №2']"
             :length="220"
 
             v-for="i in 10"
-        />
+        /> -->
     </div>
 </template>
 
@@ -19,9 +20,6 @@
         display: flex;
         flex-direction: column;
         background-color: rgb(20, 20, 20);
-        flex-grow: 1;
-        height: 1vh;
-        overflow-y: scroll;
         /* scrollbar-width: thin; */
         /* scrollbar-color:  */
     }
--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -4,29 +4,51 @@
     import PlaylistInfoCard from '@/components/Playlist/PlaylistInfoCard.vue';
     import SongsList from '@/components/SongsList.vue';
     import PlayerBar from '@/components/PlayerBar/PlayerBar.vue';
+    import Wrapper from '@/components/Wrapper.vue'
+    import TitleCard from '@/components/TitleCard.vue'
+    import Image from '@/components/Image.vue'
+    import SongCard from '@/components/SongCard.vue';
 </script>
 
 <template>
     <Header></Header>
-    <div class="wrapper">
-        <PlaylistsAside/>
-        <main>
-            <div class="playlist-main-info">
-                <PlaylistInfoCard title="Плейлист №1" description="Описание"/>
-                <SongsList/>
-                <!-- <Footer></Footer> -->
-            </div>
-        </main>
-    </div>
+    <Wrapper>
+        <div class="playlist-info">
+            <TitleCard title="Плейлист №1" created_by="username" created_at="28.11.2025">
+                <template #image>
+                    <Image/>
+                </template>
+            </TitleCard>
+            <!-- <PlaylistInfoCard title="Плейлист №1" description="Описание"/> -->
+            <SongsList>
+                <SongCard 
+                    title="Трек №1"
+                    :artists="['Исполнитель №1', 'Исполнитель №2']"
+                    :length="220"
+
+                    v-for="i in 10"
+                />
+            </SongsList>
+            <!-- <Footer></Footer> -->
+        </div>
+    </Wrapper>
     <PlayerBar/>
 </template>
 
 <style scoped>
-    .playlist-main-info{
+    .playlist-info{
+        height: 1vh;
+        flex-grow: 1;
+        background-color: rgb(20, 20, 20);
+        border-radius: 10px;
+        overflow-y: scroll;
+        padding-bottom: 10px;
+    }
+    /* .playlist-main-info{
         display: flex;
         flex-direction: column;
         gap: 15px;
         flex-grow: 1;
         height: 100%;
-    }
+    } */
 </style>
